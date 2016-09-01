24a3e5f5cbc626139d7cc85a464d1774f7b33ad8
Ramesh <rameshatdgl@gmail.com>
2016-09-01 19:27:48 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter first num");
+    scanf("%d",&f);
+    printf("Enter second num");
+    scanf("%d",&s);
+    a= a-b;
+    b= a+b;
+    a=b-a;
+    printf("\nAfter swapping,firstNumber=%d",firstNumber);
+    printf("After swapping,secondNumber=%d",secondNumber);
+    return 0;
+}
